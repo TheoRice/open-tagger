1d21645f6d0e51bfe0fc21ab284a2538d0f37bce
Theo Rice <theo.rice1996@gmail.com>
2018-09-18 17:20:05 -0400

    changed ffmpeg to use AtomicParsley in order to edit iTunes related fields

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
+const {spawn} = require('child_process');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
@@ -22,21 +23,42 @@ app.on('ready', () => {
 
 ipcMain.on('video:submit', (event, path, title, image, desc) => {
 
-    ffmpeg(path)
-        .inputOptions(`-i ${image}`)
-        .outputOptions(
-            '-map', '0:0',
-            '-map', '1:0',
-            '-c', 'copy',
-            '-id3v2_version', '4',
-            '-metadata', `title=${title}`,
-            '-metadata', `comment=${desc}`
-        )
-        .save('test.mp4')
-        .on('start', function (cmdline) {
-            console.log('Command line: ' + cmdline);
-        });
+    // ffmpeg(path)
+    //     .inputOptions(`-i ${image}`)
+    //     .outputOptions(
+    //         '-map', '0:0',
+    //         '-map', '1:0',
+    //         '-c', 'copy',
+    //         '-id3v2_version', '4',
+    //         '-metadata', `title=${title}`,
+    //         '-metadata', `comment=${desc}`
+    //     )
+    //     .save('test.mp4')
+    //     .on('start', function (cmdline) {
+    //         console.log('Command line: ' + cmdline);
+    //     });
 
+    if (image) {
+        changeArtwork(path, image);
+    }
     
-
 })
+
+function receiveSTD(ap) {
+    ap.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    ap.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    ap.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
+
+function changeArtwork(filePath, imgPath) {
+    var ap = spawn('AtomicParsley', [filePath, `--artwork`, `REMOVE_ALL`, `--artwork`, imgPath, `--overWrite`]);
+    receiveSTD(ap);
+};
